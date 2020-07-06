e39272e15bcb758cd14064452f64cdcce7b09fc4
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-06 14:36:22 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,9 +2,11 @@ def find_max_value(array)
   counter=0
   counter2=1
   while counter < array.length do
-    puts array[counter2]
     counter += 1
-    
+  if array[counter]>array[counter2] do
+    puts array[counter]
+  elseif array[counter2]>array[counter]do
+    puts array[counter2]
   end
 end
 
