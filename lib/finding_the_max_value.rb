ff150cfac7af807c44b5dbf67fd287b1b38884e6
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-06 15:10:01 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,13 +1,8 @@
-def find_max_value(array)
+def find_max_value(array, max_value)
   counter=0
   while counter < array.length do
-    array[counter]< array[counter+1]
-  if false 
-    array
+    array[counter]==max_value
     counter += 1
+  
   end
 end
-
-array=[1,5,4,2,3,3,4,2,1]
-max_value= 5
-find_max_value(array,max_value)
